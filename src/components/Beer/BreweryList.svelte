9d62619ca1127a414ddc6459ee67049101c778f4
Jim Strother <jim.strother@gmail.com>
2021-02-18 20:05:38 -0600

    Fix location error Now searches by city AND state to make sure you are not given breweries that are in another city of the same name, but in a different state. Also, now filters out breweries that don't have an address in the database and any where people cannot go to and drink at.

--- src/components/Beer/BreweryList.svelte
+++ src/components/Beer/BreweryList.svelte
@@ -7,18 +7,25 @@
       `https://maps.googleapis.com/maps/api/geocode/json?latlng=${$userCoords}&result_type=locality&key=${$apiKey}`
     )
       .then((res) => res.json())
-      .then((data) => data.results[0].address_components[0].long_name)
-      .then((city) => getBreweryList(city))
+      .then((data) => {
+        return {
+          city: data.results[0].address_components[0].long_name,
+          state: data.results[0].address_components[2].long_name
+        };
+      })
+      .then(({city, state}) => getBreweryList(city, state))
       .catch((err) => {
         throw new Error(err);
       });
   };
 
   // to find breweries
-  const getBreweryList = async (city) => {
+  const getBreweryList = async (city, state) => {
     return await fetch(
       `https://api.openbrewerydb.org/breweries?per_page=25&sort=name&by_city=${
         city.replace(' ', '_').toLowerCase()
+      }&by_state=${
+        state.replace(' ', '_').toLowerCase()
       }`
     )
       .then((res) => res.json())
@@ -39,7 +46,12 @@
     <p>Finding your location and local breweries...</p>
   {:then list}
     {#each list as brewery (brewery.id)}
-      {#if brewery.brewery_type !== "planning"}
+      {#if brewery.street // filters out breweries that don't have an address in the database
+        && brewery.brewery_type !== "planning" // these lines filter out breweries we can't go to and drink at
+        && brewery.brewery_type !== "closed" 
+        && brewery.brewery_type !== "contract" 
+        && brewery.brewery_type !== "proprietor"
+      }
         <Brewery {brewery} />
       {/if}
     {/each}
